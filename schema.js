64b9d85cc362e22913df0b3fb97260c4ecea93f8
koobawuah <me@bawuahboakye.com>
2022-03-29 23:14:42 +0000

    New schema fields for User list model, added password field and made the email field a unique field

--- schema.js
+++ schema.js
@@ -1,34 +1,42 @@
-import { list } from '@keystone-6/core'
-import { relationship, select, text, timestamp } from '@keystone-6/core/fields'
+import { list } from "@keystone-6/core";
+import {
+  password,
+  relationship,
+  select,
+  text,
+  timestamp,
+} from "@keystone-6/core/fields";
 
 export const User = list({
-    fields: {
-        name: text({ validation: { isRequired: true } }),
-        email: text({ isIndexed: true, validation: { isRequired: true }}),
-        posts: relationship({ ref: 'Post.author', many: true }),
-    }
-})
- 
+  fields: {
+    name: text({ validation: { isRequired: true } }),
+    email: text({ isIndexed: "unique", validation: { isRequired: true } }),
+    password: password({ validation: { isRequired: true } }),
+    posts: relationship({ ref: "Post.author", many: true }),
+  },
+});
 
 export const Post = list({
-    fields: {
-        title: text(),
-        author: relationship({ ref: 'User.posts', ui: {
-            displayMode: 'cards',
-            cardFields: [ 'name', 'email' ],
-            inlineCreate: { fields: [ 'name', 'email' ] },
-            linkToItem: true,
-            inlineEdit: { fields: [ 'name', 'email' ] },
-        },
-        }),
-        pusblishedAt: timestamp(),
-        status: select({
-            defaultValue: 'draft',
-            options: [
-                { label: 'Published', value: 'published' },
-                { label: 'Draft', value: 'draft' },
-            ], 
-            ui: { displayMode: 'segmented-control' },
-        }),  
-    }
-})
+  fields: {
+    title: text(),
+    author: relationship({
+      ref: "User.posts",
+      ui: {
+        displayMode: "cards",
+        cardFields: ["name", "email"],
+        inlineCreate: { fields: ["name", "email"] },
+        linkToItem: true,
+        inlineEdit: { fields: ["name", "email"] },
+      },
+    }),
+    pusblishedAt: timestamp(),
+    status: select({
+      defaultValue: "draft",
+      options: [
+        { label: "Published", value: "published" },
+        { label: "Draft", value: "draft" },
+      ],
+      ui: { displayMode: "segmented-control" },
+    }),
+  },
+});
